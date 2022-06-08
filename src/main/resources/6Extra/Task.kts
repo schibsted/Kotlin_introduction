0c7dd9556b94bbb1123fb2ca6481acf155e0c1e3
linus.ahlin-hamberg@schibsted.com <linus.ahlin-hamberg@schibsted.com>
2022-06-08 08:26:01 +0200

    Update Task.kts

--- src/main/resources/6Extra/Task.kts
+++ src/main/resources/6Extra/Task.kts
@@ -2,3 +2,6 @@
 // that takes a string and or other objects
 // and does/calls different functions depending
 // on the input.
+
+
+//Or continue on some other advent of code problems: https://kotlinlang.org/docs/advent-of-code.html
